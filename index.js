5479681af69ce3e3db2a70f92eed3fe8434aa4e4
Felipe G. Santos <felipegoncalvessantos@hotmail.com>
2017-11-07 00:26:37 -0200

    update all the index

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var router = express.Router();
+var formidable = require('formidable');
+var fs = require('fs');
 
 var path = __dirname + '/';
 
@@ -10,8 +12,21 @@ app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 
 router.get('/',function(req, res){
-  res.sendFile(path + 'index.html');
+    res.sendFile(path + 'index.html');
 });
+
+router.post('/fileupload', function(req, res){
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+        var oldpath = files.filetoupload.path;
+        var newpath = '/home/felipegsantos/Documentos/' + files.filetoupload.name;
+        fs.rename(oldpath, newpath, function (err) {
+            if (err) throw err;
+            res.write('arquivo uploadeado!');
+            res.end();
+        });
+    });
+})
 
 app.use('*', function (req, res) {
     res.send('Error 404: Not Found!');
