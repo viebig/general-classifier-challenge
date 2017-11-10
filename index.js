a37ceeef2f38f61caaf0f9e57ab9ab02fb405eb4
Felipe G. Santos <felipegoncalvessantos@hotmail.com>
2017-11-10 08:08:29 -0200

    trying new things

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var fs = require('fs');
 var path = __dirname + '/';
 
 app.use('/', router);
-app.use('/', express.static(__dirname + '/www')); // redirect root
+app.use('/', express.static(__dirname + '/www'));
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 
@@ -18,15 +18,36 @@ router.get('/',function(req, res){
 router.post('/fileupload', function(req, res){
     var form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, files) {
-        var oldpath = files.filetoupload.path;
-        var newpath = '/home/felipegsantos/Documentos/' + files.filetoupload.name;
-        fs.rename(oldpath, newpath, function (err) {
-            if (err) throw err;
-            res.write('arquivo uploadeado!');
+        var messages = files.filetoupload.path;
+        var processed = '/home/felipegsantos/Documentos/' + files.filetoupload.name;
+        //var read_stream = fs.createReadStream( messages, { encoding: 'utf-8', bufferSize: 64 * 1024, flag: 'r' });
+        
+        fs.readFile(messages, { encoding: "utf8", flag: 'r'}, function (err) {
+            if(err) throw err;
+            res.write(messages);
             res.end();
-        });
+        })
+
+        //read_stream.on("data", function (data) {
+        //    process.stdout.write(data);
+        //});
+
+        //read_stream.on("error", function (err) {
+        //    console.error("An error occurred: %s", err)
+        //});
+
+        //read_stream.on("close", function () {
+        //    console.log("File closed.")
+        //});
+        
+        //fs.rename(messages, processed, function (err) {
+          //  if (err) throw err;
+          //  res.write('arquivo uploadeado!');
+          //  res.end();
+        //});
     });
-})
+});
+
 
 app.use('*', function (req, res) {
     res.send('Error 404: Not Found!');
