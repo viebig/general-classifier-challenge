f98e83e3bd42e491a4ecc2fc65ca24a1e3b2f3eb
Felipe G. Santos <felipegoncalvessantos@hotmail.com>
2017-11-14 00:14:43 -0200

    fix server, upload two files

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 var express = require('express');
 var app = express();
 var router = express.Router();
@@ -15,44 +16,21 @@ router.get('/',function(req, res){
     res.sendFile(path + 'index.html');
 });
 
-router.post('/fileupload', function(req, res){
+router.post('/fileupload', function (req, res) {
     var form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, files) {
-        var messages = files.filetoupload.path;
-        var processed = '/home/felipegsantos/Documentos/' + files.filetoupload.name;
-        //var read_stream = fs.createReadStream( messages, { encoding: 'utf-8', bufferSize: 64 * 1024, flag: 'r' });
-        
-        fs.readFile(messages, { encoding: "utf8", flag: 'r'}, function (err) {
-            if(err) throw err;
-            res.write(messages);
+            var csv = files.csvtoupload.path;
+            var txt = files.txttoupload.path;
+
+            res.write(csv);
+            res.write(txt);
             res.end();
-        })
-
-        //read_stream.on("data", function (data) {
-        //    process.stdout.write(data);
-        //});
-
-        //read_stream.on("error", function (err) {
-        //    console.error("An error occurred: %s", err)
-        //});
-
-        //read_stream.on("close", function () {
-        //    console.log("File closed.")
-        //});
-        
-        //fs.rename(messages, processed, function (err) {
-          //  if (err) throw err;
-          //  res.write('arquivo uploadeado!');
-          //  res.end();
-        //});
-    });
-});
-
+        });
+})
 
 app.use('*', function (req, res) {
     res.send('Error 404: Not Found!');
 });
 
-app.listen(3000, function () {
-    console.log('Server is running on port 3000');
-});
+app.listen(3000);
+console.log("listen on port 3000!")
