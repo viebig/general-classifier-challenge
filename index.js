5dfa7fdf23931491fda6d7cc3a58bb4635b77f06
Felipe G. Santos <felipegoncalvessantos@hotmail.com>
2017-11-24 00:59:22 -0200

    fix conflit, update type variables

--- index.js
+++ index.js
@@ -1,40 +1,42 @@
-var http = require('http');
-var fs = require('fs');
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser')
-var jsonParser = bodyParser.json()
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
-var number = 0;
-
-var txtFile = fs.readFileSync(__dirname + '/in/messages.txt', 'utf8');
-var csvFile = fs.readFileSync(__dirname + '/in/classification.csv', 'utf8');
-csvFile = csvFile.split('\n');
-var csvHeader = csvFile[0].split(',');
-
-csvFile.forEach(function(element){
-    console.log(element.split());
-});
-
-
-console.log(csvHeader);
+const http = require('http');
+const fs = require('fs');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser')
+const jsonParser = bodyParser.json()
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+const txtFile = fs.readFileSync(__dirname + '/in/messages.txt', 'utf8');
+const csvFile = fs.readFileSync(__dirname + '/in/classification.csv', 'utf8');
+let number = 0;
 app.set('view engine', 'ejs');
 
+let array = csvFile.split('\n');
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    var newArr = [];
+    var header = element.split(',');
+    if(newArr[0] != header[0]) {
+        newArr[0] = header[0];
+    }
+    console.log(newArr[0]);
+}
+
 app.get('/', function(req, res) {
-    var PhraseResult = txtLine(txtFile, number);
-    res.render('index', {prhase: PhraseResult, CategoryA: 'categoria A', CategoryB: 'categoria B'});
+    let PhraseResult = txtLine(txtFile, number);
+    let CategoryResult = csvLine(csvFile);
+    res.render('index', {prhase: PhraseResult, CategoryA: CategoryResult[0], CategoryB: CategoryResult[1]});
 });
 
 app.post('/', urlencodedParser, function (req, res) {
-    var turn = req.body.action;
-    var cats = req.body.category;
-    var catHeader = csvLine(csvFile);
+    let turn = req.body.action;
+    let cats = req.body.category;
+    let catHeader = csvLine(csvFile);
     
     number = parseInt(number) + parseInt(turn);
     if(number == -1 ) {
         number = 0;
     }
-    var PhraseResult = txtLine(txtFile, number);
+    let PhraseResult = txtLine(txtFile, number);
     
     if(cats == "catA") {
         catHeader = bringSub(catHeader, 0, csvFile);
@@ -47,8 +49,8 @@ app.post('/', urlencodedParser, function (req, res) {
 });
 
 function txtLine(txtFile, index) {
-    var txtParse = txtFile.split("\n"); 
-    var txtElement = txtParse[index];
+    let txtParse = txtFile.split("\n"); 
+    let txtElement = txtParse[index];
     if (txtElement == undefined) {
         txtElement = "fim do arquivo alcançado";
     }
@@ -56,22 +58,21 @@ function txtLine(txtFile, index) {
 }
 
 function csvLine(csvFile) {
-    var allTextLines = csvFile.split(/\r\n|\n/);
-    var headers = allTextLines[0].split(',');
+    let allTextLines = csvFile.split(/\r\n|\n/);
+    let headers = allTextLines[0].split(',');
     
     return headers;
 }
 
 function bringSub(catHeader, index, csvFile){
-    var takeSub = csvFile.split(/\r\n|\n/);
-    var headers = takeSub[0].split(',');
-    var lines = [];
-    var tests = [];
-    for (var i = 1; i < takeSub.length; i++) {
+    let takeSub = csvFile.split(/\r\n|\n/);
+    let headers = takeSub[0].split(',');
+    let lines = [];
+    let tests = [];
+    for (let i = 1; i < takeSub.length; i++) {
         tests[i] = takeSub[i];
     }
     lines[catHeader[index]] = tests;
-    console.log(lines);
     // console.log('var', lines);
     
     return ["teste", "testes"];
