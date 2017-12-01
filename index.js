c78aad8d65b100e9548aa23b19df520085382793
Felipe G. Santos <felipegoncalvessantos@hotmail.com>
2017-12-01 00:21:43 -0200

    update index

--- index.js
+++ index.js
@@ -8,41 +8,39 @@ const jsonParser = bodyParser.json()
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
 const txtFile = fs.readFileSync(__dirname + '/in/messages.txt', 'utf8');
 const csvFile = fs.readFileSync(__dirname + '/in/classification.csv', 'utf8');
+let data = csvFile.split('\n');
+let final = {};
 let number = 0;
 app.set('view engine', 'ejs');
-let final = {};
-var queIsso;
-csv.parse(csvFile, ',', function(err,data) {
-    if(err) throw err;
-    data.forEach(function (row) {
-        if (!final[row[0]]) {
-            final[row[0]] = [];
-        }
-        final[row[0]].push(row[1]);
-    });
+
+data.forEach(function (row) {
+    row = row.split(',');
+    if (!final[row[0]]) {
+        final[row[0]] = [];
+    }
+    final[row[0]].push(row[1]);
 });
 
 app.get('/', function (req, res) {
     let PhraseResult = txtLine(txtFile, number);
-    let CategoryResult = csvLine(csvFile);
-    res.render('index', { prhase: PhraseResult, CategoryA: CategoryResult[0], CategoryB: CategoryResult[1] });
+    let CategoryResult = csvLine(final);
+    res.render('index', { prhase: PhraseResult, CategoryA: CategoryResult[0], CategoryB: CategoryResult[1]});
 });
 
 app.post('/', urlencodedParser, function (req, res) {
     let turn = req.body.action;
     let cats = req.body.category;
-    let catHeader = csvLine(csvFile);
     
     number = parseInt(number) + parseInt(turn);
     if (number == -1) {
         number = 0;
     }
     let PhraseResult = txtLine(txtFile, number);
-    
-    if (cats == "catA") {
-        catHeader = bringSub(catHeader, 0, csvFile);
-    } else if (cats == "catB") {
-        catHeader = bringSub(catHeader, 1, csvFile);
+
+    if (cats in final){
+        catHeader = final[cats];
+    } else {
+        catHeader = csvLine(final);
     }
     
     res.render('index', { prhase: PhraseResult, CategoryA: catHeader[0], CategoryB: catHeader[1] });
@@ -59,25 +57,10 @@ function txtLine(txtFile, index) {
 }
 
 function csvLine(csvFile) {
-    let allTextLines = csvFile.split(/\r\n|\n/);
-    let headers = allTextLines[0].split(',');
-    
-    return headers;
+    // let allTextLines = csvFile.split(/\r\n|\n/);
+    // let headers = allTextLines[0].split(',');
+     return ['headers', 'ghfg'];
 }
-
-function bringSub(catHeader, index, csvFile) {
-    let takeSub = csvFile.split(/\r\n|\n/);
-    let headers = takeSub[0].split(',');
-    let lines = [];
-    let tests = [];
-    for (let i = 1; i < takeSub.length; i++) {
-        tests[i] = takeSub[i];
-    }
-    lines[catHeader[index]] = tests;
-    
-    return ["teste", "testes"];
-}
-
 
 app.listen(5000);
 console.log("listen on port 5000!"); 
